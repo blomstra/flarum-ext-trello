1b3674e0209b00fbb7fdc284665ebb53ba7ca468
Ian Morland <ian@morland.me>
2021-10-22 15:31:38 +0100

    Move Trello button outside of reply dropdown

--- js/src/forum/extend/extendDiscussions.js
+++ js/src/forum/extend/extendDiscussions.js
@@ -2,7 +2,7 @@ import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import Model from 'flarum/Model';
 import Discussion from 'flarum/common/models/Discussion';
-import DiscussionControls from 'flarum/forum/utils/DiscussionControls';
+import DiscussionPage from 'flarum/forum/components/DiscussionPage';
 import SendToTrelloModal from '../components/SendToTrelloModal';
 import Button from 'flarum/common/components/Button';
 import LinkButton from 'flarum/common/components/LinkButton';
@@ -11,7 +11,8 @@ export default function () {
   Discussion.prototype.trelloCardId = Model.attribute('trelloCardId');
   Discussion.prototype.canAddToTrello = Model.attribute('canAddToTrello');
 
-  extend(DiscussionControls, 'moderationControls', function (items, discussion) {
+  extend(DiscussionPage.prototype, 'sidebarItems', function (items) {
+    const discussion = this.discussion;
     if (!discussion.canAddToTrello()) return;
 
     const trelloId = discussion.trelloCardId();
@@ -20,13 +21,14 @@ export default function () {
     items.add(
       'blomstra-trello',
       trelloId ? (
-        <LinkButton icon={faIcon} href={`https://trello.com/c/${trelloId}`} external={true} target="_blank">
+        <LinkButton className="Button" icon="fab fa-trello" href={`https://trello.com/c/${trelloId}`} external={true} target="_blank">
           {app.translator.trans('blomstra-trello.forum.controls.view_on_trello')}
         </LinkButton>
       ) : (
         Button.component(
           {
             icon: faIcon,
+            className: "Button",
             onclick: () => app.modal.show(SendToTrelloModal, { discussion }),
           },
           app.translator.trans('blomstra-trello.forum.controls.send_to_trello_button')
