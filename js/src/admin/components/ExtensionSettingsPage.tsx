7acb5259bdf9bc650b41931da4099ddb838001d4
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2021-11-23 00:17:15 +0800

    fix: Add `Board Settings` header

--- js/src/admin/components/ExtensionSettingsPage.tsx
+++ js/src/admin/components/ExtensionSettingsPage.tsx
@@ -145,8 +145,8 @@ export default class TrelloSettingsPage extends ExtensionPage {
           </div>
           <hr />
           <div class="Form-group">
+            <h3>{app.translator.trans('blomstra-trello.admin.settings.label.boards')}</h3>
             <label>{app.translator.trans('blomstra-trello.admin.settings.available_boards_label')}</label>
-
             {this.states.allBoards === null ? (
               <LoadingIndicator />
             ) : this.states.allBoards ? (
