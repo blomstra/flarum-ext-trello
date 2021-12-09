1b2885a07cd26f087463fa4c68d7525890087f93
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2021-12-09 21:02:37 +0800

    fix: Extract Trello colors into a separate file

--- js/src/admin/components/ExtensionSettingsPage.tsx
+++ js/src/admin/components/ExtensionSettingsPage.tsx
@@ -9,6 +9,7 @@ import tagLabel from 'flarum/tags/helpers/tagLabel';
 import sortTags from 'flarum/tags/utils/sortTags';
 import saveSettings from 'flarum/admin/utils/saveSettings';
 import type Mithril from 'mithril';
+import colors from '../../common/colors';
 
 export interface Board {
   organization(): string;
@@ -213,20 +214,6 @@ export default class TrelloSettingsPage extends ExtensionPage {
   content() {
     function displayLabel(label: {}) {
       let attrs = {};
-
-      // Trello color choices
-      const colors = {
-        green: '#61bd4f',
-        yellow: '#f2d600',
-        orange: '#ff9f1a',
-        red: '#eb5a46',
-        purple: '#c377e0',
-        blue: '#0079bf',
-        sky: '#00c2e0',
-        lime: '#51e898',
-        pink: '#ff78cb',
-        black: '#344563',
-      };
 
       attrs.style = {};
       attrs.className = 'TagLabel ';
---
+++ js/src/common/colors.ts
@@ -0,0 +1,15 @@
+// Trello color choices
+const colors = {
+  green: '#61bd4f',
+  yellow: '#f2d600',
+  orange: '#ff9f1a',
+  red: '#eb5a46',
+  purple: '#c377e0',
+  blue: '#0079bf',
+  sky: '#00c2e0',
+  lime: '#51e898',
+  pink: '#ff78cb',
+  black: '#344563',
+};
+
+export default colors;
