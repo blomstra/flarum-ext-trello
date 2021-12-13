3c9876b375ae3d8efbb93e178c4544cd85eebf8b
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2021-12-13 21:26:10 +0800

    Use ===
    Co-authored-by: David Wheatley <hi@davwheat.dev>

--- src/Controllers/DeleteBoardController.php
+++ src/Controllers/DeleteBoardController.php
@@ -40,7 +40,7 @@ class DeleteBoardController extends AbstractDeleteController
 
         $defaultBoardId = $this->settings->get('blomstra-trello.default_board_id');
 
-        if ($shortLink == $defaultBoardId) {
+        if ($shortLink === $defaultBoardId) {
             $this->settings->delete('blomstra-trello.default_board_id');
             $this->settings->delete('blomstra-trello.last_used_lane_id');
         }
