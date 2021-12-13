f50a999ddafcf69c9a714cdce688ba3efbc258b4
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2021-12-13 21:24:18 +0800

    fix: refactor references for $client

--- src/Controllers/ListBoardMembersController.php
+++ src/Controllers/ListBoardMembersController.php
@@ -12,53 +12,44 @@
 namespace Blomstra\Trello\Controllers;
 
 use Blomstra\Trello\Serializer\TrelloMemberSerializer;
-use Blomstra\Trello\ValidateTrelloSettings;
 use Exception;
 use Flarum\Api\Controller\AbstractListController;
 use Flarum\Http\RequestUtil;
-use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Support\Arr;
 use Psr\Http\Message\ServerRequestInterface;
 use Tobscure\JsonApi\Document;
-use Trello\Client;
+use Trello\Client as TrelloClient;
 use Trello\Models\Board;
 
 class ListBoardMembersController extends AbstractListController
 {
     /**
-     * @var SettingsRepositoryInterface
+     * @var TrelloClient
      */
-    protected $settings;
+    protected $client;
 
     /**
      * {@inheritdoc}
      */
     public $serializer = TrelloMemberSerializer::class;
 
-    public function __construct(SettingsRepositoryInterface $settings)
+    public function __construct(TrelloClient $client)
     {
-        $this->settings = $settings;
+        $this->client = $client;
     }
 
     protected function data(ServerRequestInterface $request, Document $document)
     {
         RequestUtil::getActor($request)->assertAdmin();
 
-        if (!ValidateTrelloSettings::Settings($this->settings)) {
+        if (!$this->client) {
             return [];
         }
 
         try {
             $board = Arr::get($request->getQueryParams(), 'board');
 
-            $apiKey = $this->settings->get('blomstra-trello.api_key');
-            $apiToken = $this->settings->get('blomstra-trello.api_token');
-
-            $client = new Client($apiKey);
-
-            $client->setAccessToken($apiToken);
-
-            $board = (new Board($client))->setId($board)->get();
+            $board = (new Board($this->client))->setId($board)->get();
 
             return $board->getMembers();
         } catch (Exception $e) {
